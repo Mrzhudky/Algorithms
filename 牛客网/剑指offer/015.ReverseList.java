9ceff74980fe0f303bc91ebdaec671ad5773a800
Mrzhudky <1183260441@qq.com>
2018-03-26 21:20:30 +0800

    反转链表

---
+++ 牛客网/剑指offer/015.ReverseList.java
@@ -0,0 +1,56 @@
+package com.company;
+
+/**
+ * 问题：输入一个链表，反转链表后，输出链表的所有元素
+ *
+ * 思路：三个指针，一个指向当前要反转的节点，一个指向原链表下一个节点，一个指向反转后链表头结点
+ */
+
+
+class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+class Solution {
+    public ListNode ReverseList(ListNode head) {
+        ListNode thisNode = head;
+        if(thisNode == null){
+            return null;
+        }
+        if(thisNode.next == null){
+            return thisNode;
+        }
+        ListNode revHead = null;
+        ListNode preNode = thisNode.next;
+        while (preNode.next != null){
+            thisNode.next = revHead;
+            revHead = thisNode;
+            thisNode = preNode;
+            preNode = preNode.next;
+        }
+        thisNode.next = revHead;
+        revHead = thisNode;
+        preNode.next = revHead;
+        revHead = preNode;
+        return revHead;
+    }
+}
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] inputData = new int[]{1,3,5,7,2,4,6};
+
+        for (int data :inputData
+             ) {
+            System.out.print(data);
+        }
+
+    }
+}
