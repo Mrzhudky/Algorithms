5cd2ac0315ed7c5c65fd36f9563bbc31b9978d13
Mrzhudky <1183260441@qq.com>
2018-04-23 21:31:35 +0800

    删除了多余文件

--- untitled/src/com/company/Main.java
+++ untitled/src/com/company/Main.java
@@ -1,54 +1,80 @@
 package com.company;
-import java.util.Scanner;
+import java.util.*;
+
 /**
- * 题目名称：最长公共子串
- *输入描述：随机输入两个字符串，都是英文字母，不区分大小写
+ *题目描述
+ *在数组中的两个数字，如果前面一个数字大于后面的数字，则这两个数字组成一个逆序对。输入一个数组,求出这个数组中的逆序对的总数P。并将P对1000000007取模的结果输出。 即输出P%1000000007
+ *输入描述:
+ *题目保证输入的数组中没有的相同的数字
  *
- *输出描述：输出“resullt=?”，没有匹配到返回“”，匹配到返回“slfl”字样
+ *数据范围：
+ *
+ *对于%50的数据,size<=10^4
+ *
+ *对于%75的数据,size<=10^5
+ *
+ *对于%100的数据,size<=2*10^5
+ *
  *示例1
  *输入
- *afhelk afdzhelos
+ *1,2,3,4,5,6,7,0
  *输出
- *resullt=hel
+ *7
+ *
  */
-
-
-
 
 public class Main {
 
+    public int InversePairs(int [] array) {
+        if(array == null || array.length == 0){
+            return -1;
+        }
+        int[] helpArray = new int[array.length];
+        System.arraycopy(array,0,helpArray,0,array.length);
+        long result = 0;
+        result = mergeSortModify(array,helpArray,0,array.length-1);
+        result = result%1000000007;
+        return (int)result;
+    }
 
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        char[] str1 = sc.next("\\w+").toCharArray();
-        char[] str2 = sc.next("\\w+").toCharArray();
-
-        int[][] dp = new int[str1.length][str2.length];
-        int maxLen = 0;
-        int endI = 0;
-        for (int i = 0; i < str1.length; i++) {
-            for (int j = 0; j < str2.length; j++) {
-                if(str1[i] == str2[j]){
-                    if(i>0 && j>0){
-                        dp[i][j] = dp[i-1][j-1] +1;
-                    }
-                    else{
-                        dp[i][j] = 1;
-                    }
-                }
-                if(dp[i][j]>maxLen){
-                    maxLen = dp[i][j];
-                    endI = i;
-                }
+    //使用了归并排序的思想
+    long mergeSortModify(int[] array,int[] helpArray,int start,int end){
+        if(start>end){
+            return 0;
+        }
+        if(start == end){
+            helpArray[start] = array[start];
+            return 0;
+        }
+        int middle = (start + end)/2;
+        // 这里有一个交换array和helpArray的细节需要注意
+        long leftNum = mergeSortModify(helpArray,array,start,middle);
+        long rigthNum = mergeSortModify(helpArray,array,middle+1,end);
+        long sum = leftNum + rigthNum;
+        int i = middle;
+        int j =  end;
+        int index = end;
+        while ((i>=start)&&(j>middle)){
+            if(array[i]>array[j]){
+                helpArray[index--] = array[i--];
+                sum += j-middle;
+            }else{
+                helpArray[index--] = array[j--];
             }
         }
-        System.out.print("result=");
-        for (int i = endI - maxLen +1; i <= endI; i++) {
-            System.out.print(str1[i]);
+        while (j>middle){
+            helpArray[index--] = array[j--];
         }
-        System.out.println();
+        while (i>=start){
+            helpArray[index--] = array[i--];
+        }
+        return sum;
+    }
 
-
+    public static void main(String[] args) {
+        Main m = new Main();
+        int[] array = new int[]{1,2,3,0};
+        int result = m.InversePairs(array);
+        System.out.println(result);
     }
 }
--- untitled/src/com/company/SolutionTest.java
+++ untitled/src/com/company/SolutionTest.java
@@ -2,12 +2,14 @@ package com.company;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class SolutionTest {
     @Test
     public void test() throws Exception {
-       // Solution solution = new Solution();
+
 
     }
 
