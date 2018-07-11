d659f2652e0d7546b0dd5614bb4687bed3aef1a7
Mrzhudky <1183260441@qq.com>
2018-07-11 15:49:12 +0800

    只出现一次的数，通过1ms

---
+++ leetCode/初级算法/数组/SingleNumber.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.util.*;
+
+/**
+ 只出现一次的数字
+ 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+
+ 说明：
+
+ 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+
+ 示例 1:
+
+ 输入: [2,2,1]
+ 输出: 1
+ 示例 2:
+
+ 输入: [4,1,2,1,2]
+ 输出: 4
+ *
+ * 思路：
+ * 异或，出现两次的异或为0，只剩下出现一次的。
+ */
+
+public class Main {
+
+    public int singleNumber(int[] nums) {
+        if(nums == null || nums.length==0){
+            return 0;
+        }
+        int result = 0;
+        for(int i=0;i<nums.length;i++){
+            result = result^nums[i];
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
